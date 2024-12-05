e6ee6645890620aa49cbd861fd330111dab001a2
Guilhermedelaporta <guilhermedelaporta1@gmail.com>
2024-12-04 22:54:33 -0300

    t
    .

---
+++ main.py
@@ -0,0 +1,19 @@
+import pandas as pd  #Importando a biblioteca pandas e apelidando de pd
+
+tabela_vendas = pd.read_excel("Vendas.xlsx") # Importando a base de dados e criando a tabela de vendas e armazenando ela em uma variável 
+# print(tabela_vendas) # Testando 
+
+pd.set_option('display.max.columns', None) # 
+print(tabela_vendas)
+
+# importar a base de dados
+ 
+# visualizar base de dados 
+ 
+# faturamento por loja
+
+# quantidade de produtos vendidos por lojas 
+
+# ticket medio por produto em cada loja 
+
+# enviar um email com o relatório 
